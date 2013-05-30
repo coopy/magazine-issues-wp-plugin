ec107b2158739f3d0b11b37a1fc3a154413cfe22
Per Nilsson <pnilsson@zynga.com>
2013-05-29 21:33:28 -0700

    Don't show issues as a taxonomy option in the menu bar.

--- magazine-issues.php
+++ magazine-issues.php
@@ -116,7 +116,7 @@ if (!class_exists('MagazineIssues')) {
                 'hierarchical' => false,
                 'label' => 'Issues',
                 'labels' => $taxonomyLabels,
-                'show_ui' => true,
+                'show_ui' => false,
                 'show_admin_column' => true,    // TODO
                 'update_count_callback' => '_update_post_term_count',
                 'query_var' => true,            // TODO
